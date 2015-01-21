e77271739ac7170a10dba3e38900df98d5a10ce5
vbganesh <vijayganesh999@gmail.com>
2015-01-21 11:57:34 -0500

    added all project files

---
+++ Assets/scripts/Meshstuff.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var newVertices : Vector3[];
+var newUV : Vector2[];
+var newTriangles : int[];
+
+function Start () {
+	Debug.Log("DFGDFGDFGDFG");
+	var mesh : Mesh = new Mesh ();
+	GetComponent(MeshFilter).mesh = mesh;
+	mesh.vertices = newVertices;
+	mesh.uv = newUV;
+	mesh.triangles = newTriangles;
+}
+
+function Update () {
+	var mesh : Mesh = GetComponent(MeshFilter).mesh;
+	var vertices : Vector3[] = mesh.vertices;
+	var normals : Vector3[] = mesh.normals;
+	for (var i = 0; i < vertices.Length; i++)
+		vertices[i] += normals[i] * Mathf.Sin(Time.time);
+	mesh.vertices = vertices;
+}
